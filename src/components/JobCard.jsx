56248825e1d7e5e90525cc116c26a829d2290768
Harshroxnox <harshroxnox@gmail.com>
2025-09-21 08:28:13 +0530

    completed job board

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
-import logo from '../assets/microsoft.webp'
 import { MapPin, Briefcase  } from 'lucide-react'
 
-const JobCard = (props) => {
+const JobCard = (data) => {
   return (
-    <div className='flex flex-col gap-3 w-[28vw] p-8 bg-secondary rounded-2xl'>
+    <div className={`flex flex-col gap-3 w-[28vw] p-8 ${data.bg} rounded-2xl`}>
       <div className='flex items-center justify-between'>
         <div>
-          <div className='text-lg font-normal text-teal-50'>Senior UI Designer</div>
-          <div className='font-normal'>Microsoft</div>
+          <div className='text-lg font-normal text-teal-50'>{data.role}</div>
+          <div className='font-normal'>{data.company}</div>
         </div>
         <div className='h-12 w-12'>
-          <img src={logo} alt="" />
+          <img src={data.logo} alt="" />
         </div>
       </div >
-      <div>Take advantage of a rare opportunity to start from the ground up and build..</div>
+      <div>{data.desc}</div>
       <div className='flex items-center justify-start gap-3'>
-        <div className='flex gap-1'> <MapPin /> <p>Glendale, CA</p></div>
-        <div className='flex gap-2'> <Briefcase /> <p>Full-time</p></div>
+        <div className='flex gap-1'> <MapPin /> <p>{data.location}</p></div>
+        <div className='flex gap-2'> <Briefcase /> <p>{data.type}</p></div>
       </div>
       <div className='flex items-center justify-between'>
         <div>
-          <div className='text-lg font-normal'>20K</div>
-          <div>Monthly</div>
+          <div className='text-lg font-normal'>{data.salary}</div>
+          <div>{data.period}</div>
         </div>
         <div className='px-16 py-3 border-1 font-bold rounded-full'>Candidates</div>
       </div>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import JobCard from '../components/JobCard'
+import microsoft from '../assets/microsoft.webp'
+import behance from '../assets/behance.jpg'
+import mailchimp from '../assets/mailchimp.png'
 
 const LandingPage = () => {
   return (
@@ -25,14 +28,33 @@ const LandingPage = () => {
       </div>
       <hr className='h-[0.1rem] mx-40 mt-10 border-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'/>
     
-      {/* ------------------------------------------------------------------------------------------ */}
+      {/* --------------------------------------Job-Board------------------------------------------------ */}
       <div>
         <h1 className="text-4xl my-10 font-bold">Manage Hiring</h1>
-        <div>
-          <JobCard hello={"d"}/>
+        <div className='flex items-center justify-between'>
+          <JobCard 
+            role="Senior UI Designer" company="Microsoft" bg="bg-secondary"
+            desc="Take advantage of a rare opportunity to start from the ground up and build.."
+            location="Glendale, CA" type="Full-time" salary="$20K" period="Monthly" logo={microsoft}
+          />
+          <JobCard
+            role="Product Designer" company="Behance"  bg="bg-[#211d24ff]"
+            desc="The Sr Product designer will be responsible for creating production assets.."
+            location="Andover, MA" type="Remote" salary="$25K" period="Monthly" logo={behance}
+          />
+          <JobCard 
+            role="Marketing Officer" company="Mailchimp"  bg="bg-[#1d241dff]"
+            desc="The Marketing will work as part of a global, cross functional team that creates"
+            location="Weston, MA" type="Full-time" salary="$15K" period="Monthly" logo={mailchimp}
+          />
         </div>
       </div>
-    
+
+
+      {/* --------------------------------------Job-Board------------------------------------------------ */}
+      
+
+
     </div>
   )
 }
